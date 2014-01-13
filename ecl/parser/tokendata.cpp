2ecc2123273b8df8f7c903fa41b06a7d2b0d6203
jamienoss <james.noss@lexisnexis.com>
2014-01-13 15:38:50 +0000

    HPCC-9799 Status
    Added breaks to switch in parser/tokendata.cpp
    
    Signed-off-by: jamienoss <james.noss@lexisnexis.com>

--- ecl/parser/tokendata.cpp
+++ ecl/parser/tokendata.cpp
@@ -8,9 +8,10 @@ void TokenData::cpy(TokenData tok) // Discuss with Gavin. At present this is imp
 		unsigned txtLen = strlen(tok.lexeme) + 1;
 		lexeme = new char [txtLen];
 		memcpy(lexeme, tok.lexeme, txtLen);
+		break;
 	}
-	case (integerKind) : integer = tok.integer;
-	case (realKind) : real = tok.real;
+	case (integerKind) : integer = tok.integer; break;
+	case (realKind) : real = tok.real; break;
 	}
 
 	lineNumber = tok.lineNumber;
